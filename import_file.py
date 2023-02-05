5e4b54e5eb72d5bc49e7f1cd969be98999791869
JosephABB <benitezjoseph267@gmail.com>
2023-02-04 17:43:49 -0800

    finished import_file module

--- import_file.py
+++ import_file.py
@@ -1,19 +1,36 @@
 import csv
 
-def imp(csv_file):
-    #open main csv file
-    with open("transaction_records.csv", 'a') as file:
-         writer = csv.writer(file, lineterminator = "\n")
+def imp():
+    #which file to import
+    new_csv = input("Which file would you like to import? ")
 
-         #open new csv file
-         with open(csv_file, "r") as new_file:
-            reader = csv.reader(new_file)
-            #iterate through new file and append to old file
-            for row in reader:
-                writer.writerow(new_file)
-        
-        #close old file
-         file.close()
+    #open new file as read and old file as append
+    with open(new_csv, 'r') as in_file, open("transaction_records.csv", 'a') as out_file:
+        reader = csv.reader(in_file)
+        writer = csv.writer(out_file)
+
+        # create list of value in current records
+        list_lines = []
+        with open("transaction_records.csv", "r") as current_file:
+            reader_old = csv.reader(current_file)
+            for line in reader_old:
+                list_lines.append(line)
+        current_file.close()
+
+        #check if new file contains id's already in old file
+        for line in reader:
+            # skip duplicate
+            if any(line[0] in sublist for sublist in list_lines): 
+                print(line[0], " is a duplicate")
+                continue
+            else:
+                writer.writerow(line)
+                list_lines.append(line)
+
+    print("imported")
+
+    out_file.close()
+    in_file.close()
 
 
 
