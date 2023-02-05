191d49218f7b29b91f0b65026e36edd41fbfd77b
JosephABB <benitezjoseph267@gmail.com>
2023-02-04 18:00:04 -0800

    updated import_file module

--- import_file.py
+++ import_file.py
@@ -3,34 +3,39 @@ import csv
 def imp():
     #which file to import
     new_csv = input("Which file would you like to import? ")
+    counter = 0
 
-    #open new file as read and old file as append
-    with open(new_csv, 'r') as in_file, open("transaction_records.csv", 'a') as out_file:
-        reader = csv.reader(in_file)
-        writer = csv.writer(out_file)
+    try:
+        #open new file as read and old file as append
+        with open(new_csv, 'r') as in_file, open("transaction_records.csv", 'a') as out_file:
+            reader = csv.reader(in_file)
+            writer = csv.writer(out_file)
 
-        # create list of value in current records
-        list_lines = []
-        with open("transaction_records.csv", "r") as current_file:
-            reader_old = csv.reader(current_file)
-            for line in reader_old:
-                list_lines.append(line)
-        current_file.close()
+            # create list of value in current records
+            list_lines = []
+            with open("transaction_records.csv", "r") as current_file:
+                reader_old = csv.reader(current_file)
+                for line in reader_old:
+                    list_lines.append(line)
+            current_file.close()
 
-        #check if new file contains id's already in old file
-        for line in reader:
-            # skip duplicate
-            if any(line[0] in sublist for sublist in list_lines): 
-                print(line[0], " is a duplicate")
-                continue
-            else:
-                writer.writerow(line)
-                list_lines.append(line)
+            #check if new file contains id's already in old file
+            for line in reader:
+                # skip duplicate
+                if any(line[0] in sublist for sublist in list_lines): 
+                    print("Data for ID ", line[0], " already in transaction records.")
+                    continue
+                else:
+                    counter += 1
+                    writer.writerow(line)
+                    list_lines.append(line)
+        out_file.close()
+        in_file.close()
 
-    print("imported")
-
-    out_file.close()
-    in_file.close()
+        print(counter, "transaction records successfully loaded.")
+    except:
+        print("Sorry, that file could not be found. Please try again.") 
+        
 
 
 
